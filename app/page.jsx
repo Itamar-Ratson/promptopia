f4b1d67c3014ee3d314789b549fbac65324245bb
Itamar Ratson <itamar.ratson@gmail.com>
2024-07-19 23:57:12 +0300

    added suspense to main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,23 @@
+'use client';
+
 import Feed from '@components/Feed';
+import { Suspense } from 'react';
 
 const Home = () => (
-	<section className='w-full flex-center flex-col'>
-		<h1 className='head_text text-center'>
-			Discover & Share
-			<br className='max-md:hidden' />
-			<span className='orange_gradient text-center'> AI-Powered Prompts</span>
-		</h1>
-		<p className='desc text-center'>
-			Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative
-			prompts
-		</p>
-		<Feed />
-	</section>
+	<Suspense>
+		<section className='w-full flex-center flex-col'>
+			<h1 className='head_text text-center'>
+				Discover & Share
+				<br className='max-md:hidden' />
+				<span className='orange_gradient text-center'> AI-Powered Prompts</span>
+			</h1>
+			<p className='desc text-center'>
+				Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative
+				prompts
+			</p>
+			<Feed />
+		</section>
+	</Suspense>
 );
 
 export default Home;
